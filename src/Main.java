e933688fc5adcc0de18fb6cd1912f3f70bd9a34f
nkrenta <nkrenta@inbox.ru>
2024-08-05 10:51:04 +0300

    hw2 is done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // write your code here
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box / 4;
+        System.out.println(box);
+        float a = 1;
+        float b = 3;
+        var result1 = a + b;
+        System.out.println("result1 = " + result1);
+        var result2 = a - b;
+        System.out.println("result2 = " + result2);
+        float result3 = a / b;
+        System.out.println("result3 = " + String.format("%.3f", result3));
+        float result = a / 2 * b;
+        System.out.println("result = " + String.format("%.3f", result));
+
+        //Count available weight to use
+        Scanner lC = new Scanner(System.in);
+        System.out.println("Введите объем багажника:");
+        int liftingCapacity = lC.nextInt();
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Еще можно положить - " + capacityLeft + " кг вещей");
+
+        //Count weight of fruits
+        var appleWeight = 2;
+        var orangesWeight = 3;
+        var fruitWeight = appleWeight + orangesWeight;
+        System.out.println("Общий вес фруктов - " + fruitWeight + " кг!");
+
+        //Count weight of all products
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumberWeight = 2;
+        var peppersWeight = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumberWeight + peppersWeight + zucchiniWeight;
+        float productsWeight = fruitWeight + meatWeight + waterWeight + vegetablesWeight;
+        System.out.println("Общий вес продуктов - " + productsWeight + " кг!");
+
+        //Count left weight
+        float leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        System.out.println("Места осталось - " + leftWeight + " кг!");
+
+        //+2 people
+        productsWeight = productsWeight * 2;
+        System.out.println("Теперь вес продуктов - " + productsWeight + " кг!");
+        //Count of overload
+        //Also can use Math.abs(leftWeight)
+        leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        var overload = (stuffWeight + productsWeight) % liftingCapacity;
+        if (leftWeight < 0) {
+            System.out.println("Места в машине не достаточно! Перегруз на " + overload + " кг!");
+            System.out.println("Распределим поровну продукты по машинам");
+            Scanner cC = new Scanner(System.in);
+            System.out.println("Введите количество машин:");
+            int carCount = cC.nextInt();
+            float separateWeight = productsWeight / carCount;
+            if (carCount >= 5) {
+                System.out.println("Все " + productsWeight + " кг были распределены в " + carCount + " машин!");
+                System.out.println("Вес для одной машины составил - " + String.format("%.1f", separateWeight) + " кг.");
+            } else {
+                System.out.println("Все " + productsWeight + " кг были распределены в " + carCount + " машины!");
+                System.out.println("Вес для одной машины составил - " + String.format("%.1f", separateWeight) + " кг.");
+            }
+
+        } else {
+            System.out.println("В машине осталось - " + leftWeight + " кг!");
+        }
+
     }
 }
--- src/hw2.java
+++ src/hw2.java
@@ -1,5 +1,71 @@
 public class hw2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //First task
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("\n");
+
+        //Second task
+        dog = dog + 4;
+        cat = cat +4;
+        paper = paper + 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("\n");
+
+        //Third task
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("\n");
+
+        //Fourth task
+        var friend = 19;
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("\n");
+
+        //Fifth task
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("\n");
+
+        //Sixth task
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес боксеров равен " + totalWeight + " кг.");
+        System.out.println("\n");
+
+        //Seventh task
+        var differenceWeight = secondBoxer % firstBoxer;
+        System.out.println("Весовая разница боксеров равна " + differenceWeight + " кг.");
+        System.out.println("\n");
+
+        //Eighth task
+        var totalTime = 640;
+        var oneTime = 8;
+        var memberCount = totalTime / oneTime;
+        System.out.println("Всего работников в компании - " + memberCount + " человек.");
+
+        memberCount = memberCount + 94;
+        totalTime = memberCount * oneTime;
+        System.out.println("Если в компании работает " + memberCount + " человек, то всего " + totalTime + " часа работы может быть поделено между сотрудниками.");
+
     }
 }
